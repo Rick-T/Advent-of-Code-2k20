8d3bd6280247d74781f024ca32189cb633dad6ac
Patrick Teubner <patrick.teubner@rub.de>
2020-12-19 15:30:21 +0100

    Fun with semirings

--- src/Day19.hs
+++ src/Day19.hs
@@ -2,13 +2,20 @@ module Day19 where
 
 import Aoc.Input (readInput, withInput)
 import Aoc.Parsers (Parser, integer, parseBest)
+import Aoc.Regex (Regex (Branch, Chain, Unit), matches)
 import Control.Applicative (Alternative (some, (<|>)), optional)
-import Control.Applicative.Combinators (between, sepBy, sepEndBy)
+import Control.Applicative.Combinators
+  ( between,
+    optional,
+    sepBy,
+    sepEndBy,
+    some,
+    (<|>),
+  )
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as M
+import Data.Semiring as SR (foldMapP, foldMapT, sum)
 import Text.Megaparsec.Char (char, letterChar, newline, string)
-
-data Regex a = Unit a | Branch (Regex a) (Regex a) | Chain (Regex a) (Regex a)
 
 data Rule = Leaf Char | Tree (Regex Int)
 
@@ -16,35 +23,28 @@ type Rules = IntMap Rule
 
 data Input = Input Rules [String]
 
+-- >>> part1
+-- 132
 part1 :: IO Int
 part1 = withInput "Day19.txt" (readInput $ parseBest inputP) solvePart1
 
+-- >>> part2
+-- 306
 part2 :: IO Int
 part2 = withInput "Day19.txt" (readInput $ parseBest inputP) solvePart2
 
 solvePart1 :: Input -> Int
 solvePart1 (Input map ls) =
-  let evals = construct 0 map
-   in length $ filter (matches evals) ls
+  let regex = construct 0 map
+   in length $ filter (matches regex) ls
 
 solvePart2 :: Input -> Int
 solvePart2 (Input map ls) =
-  let evals = construct 0 (update map)
-   in length $ filter (matches evals) ls
+  let regex = construct 0 (update map)
+   in length $ filter (matches regex) ls
 
 update :: Rules -> Rules
 update rules = foldr (uncurry M.insert) rules $ parseBest ruleP <$> ["8: 42 | 42 8", "11: 42 31 | 42 11 31"]
-
-matches :: (Eq c) => Regex c -> [c] -> Bool
-matches r s = [] `elem` go r s
-  where
-    go :: Eq c => Regex c -> [c] -> [[c]]
-    go _ [] = []
-    go (Unit c) (t : s) = [s | t == c]
-    go (Branch a b) s = go a s ++ go b s
-    go (Chain a b) s =
-      let match = go a s
-       in concat [go b rest | rest <- match]
 
 construct :: Int -> Rules -> Regex Char
 construct i map = go (map M.! i)
@@ -71,7 +71,7 @@ letterRule :: Parser Rule
 letterRule = Leaf <$> between (char '"') (char '"') letterChar
 
 numberRule :: Parser Rule
-numberRule = Tree . foldr1 Branch <$> chainNumbers `sepBy` char '|'
+numberRule = Tree . SR.sum <$> chainNumbers `sepBy` char '|'
 
 chainNumbers :: Parser (Regex Int)
-chainNumbers = optional (char ' ') *> (foldr1 Chain . fmap Unit <$> integer `sepEndBy` char ' ')
+chainNumbers = optional (char ' ') *> (foldMapT Unit <$> integer `sepEndBy` char ' ')
